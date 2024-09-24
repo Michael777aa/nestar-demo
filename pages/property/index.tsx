b8214537138d57b277446f3051ce47abd5f2ee5a
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-09-24 23:31:59 +0900

    fix: refactor pages components

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -3,7 +3,11 @@ import { NextPage } from "next";
 import withLayoutBasic from "../libs/components/layout/LayoutBasic";
 
 const PropertyList: NextPage = () => {
-  return <Container>PROPERTY LIST</Container>;
+  return (
+    <div style={{ margin: "20px 0" }}>
+      <Stack className="container">PROPERTY LIST</Stack>
+    </div>
+  );
 };
 
 export default withLayoutBasic(PropertyList);
